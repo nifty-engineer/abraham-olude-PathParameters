0d299b8e51286b34c9ce2ccb641725b7b3ebbfb6
Abraham Olude <abraham.olude.ao@gmail.com>
2023-10-20 02:55:48 +0000

    Complete Coding Lab: Path Parameters

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -19,6 +19,9 @@ public class JavalinSingleton {
         app.get("/firstname/{first}", ctx -> {
             
             //write code here
+            String firstName = ctx.pathParam("first");
+
+            ctx.result(firstName);
 
         });
 
